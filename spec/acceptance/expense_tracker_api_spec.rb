b0caca2b146a008f71364268a2786c7b3a5d4dfd
Gunnar Bastkowski <gunnar@digitalstep.de>
2017-06-28 14:43:32 +0200

    Add failing test for date filter

--- spec/acceptance/expense_tracker_api_spec.rb
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -25,6 +25,24 @@ module ExpenseTracker
         'amount' => 5.75,
         'date'   => '2017-06-10'
       )
+
+      zoo = post_expense(
+        'payee'  => 'Zoo',
+        'amount' => 15.25,
+        'date'   => '2017-06-10'
+      )
+
+      groceries = post_expense(
+        'payee'  => 'Whole Foods',
+        'amount' => 95.20,
+        'date'   => '2017-06-11'
+      )
+
+      get '/expenses/2017-06-10'
+      expect(last_response.status).to eq(200)
+
+      expenses = JSON.parse(last_response.body)
+      expect(expenses).to contain_exactly(coffee, zoo)
     end
 
   end
