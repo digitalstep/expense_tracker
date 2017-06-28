4c5d53b5d137120f8d85f09b90356d671f92c9cf
Gunnar Bastkowski <gunnar@digitalstep.de>
2017-06-28 14:39:26 +0200

    Refactor test

--- spec/acceptance/expense_tracker_api_spec.rb
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -10,18 +10,21 @@ module ExpenseTracker
       ExpenseTracker::API.new
     end
 
-    it 'records submitted expenses' do
-      coffee = {
-        'payee'  => 'Starbucks',
-        'amount' => 5.75,
-        'date'   => '2017-06-10'
-      }
-
-      post '/expenses', JSON.generate(coffee)
+    def post_expense(expense)
+      post '/expenses', JSON.generate(expense)
       expect(last_response.status).to eq(200)
 
       parsed = JSON.parse(last_response.body)
       expect(parsed).to include('expense_id' => a_kind_of(Integer))
+      expense.merge('id' => parsed['expense_id'])
+    end
+
+    it 'records submitted expenses' do
+      coffee = post_expense(
+        'payee'  => 'Starbucks',
+        'amount' => 5.75,
+        'date'   => '2017-06-10'
+      )
     end
 
   end
